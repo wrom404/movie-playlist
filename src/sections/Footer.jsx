1b2dc9d604c132213a74f00e128bf48961eef595
Unkownymous99 <romwendel99@gmail.com>
2024-08-28 00:20:35 +0800

    made som changes

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <section className='h-40 sm:h-52 md:h-64 bg-dark flex flex-col justify-center items-center'>
-      <div className="text-sm text-center py-2">
+      <div className="text-xs md:text-sm text-center py-2">
         This website is currently under development and is not yet finished.
       </div>
       <p className='text-xs md:text-sm lg:text-base'>Copyright ©2024 | Created by wrom404</p>
